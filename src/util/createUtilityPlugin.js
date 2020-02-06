627be3a8fa6b7e76111654f6eb65d4dffbf595e3
Benoît Rouleau <benoit.rouleau@icloud.com>
2020-02-06 18:08:49 -0500

    Add handleDefault and handleNegative options to createUtilityPlugin()

--- src/util/createUtilityPlugin.js
+++ src/util/createUtilityPlugin.js
@@ -2,25 +2,28 @@ import fromPairs from 'lodash/fromPairs'
 import toPairs from 'lodash/toPairs'
 import castArray from 'lodash/castArray'
 
-function className(classPrefix, key) {
-  if (key === 'default') {
+function className(classPrefix, key, options) {
+  if (options.handleDefault && key === 'default') {
     return classPrefix
   }
 
-  if (key.startsWith('-')) {
+  if (options.handleNegative && key.startsWith('-')) {
     return `-${classPrefix}${key}`
   }
 
   return `${classPrefix}-${key}`
 }
 
-export default function createUtilityPlugin(themeKey, utilityVariations) {
+export default function createUtilityPlugin(themeKey, utilityVariations, {
+  handleDefault = true,
+  handleNegative = true,
+} = {}) {
   return function({ e, addUtilities, variants, theme }) {
     const utilities = utilityVariations.map(([classPrefix, properties]) => {
       return fromPairs(
         toPairs(theme(themeKey)).map(([key, value]) => {
           return [
-            `.${e(className(classPrefix, key))}`,
+            `.${e(className(classPrefix, key, { handleDefault, handleNegative }))}`,
             fromPairs(castArray(properties).map(property => [property, value])),
           ]
         })
